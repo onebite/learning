0513cf9a411b74c4c871f14d134b5e8d2f4807df
onebite <625573116@qq.com>
2016-10-20 18:58:59 +0800

    Changes to be committed: modified:   demo/.classpath modified:   demo/bin/demo/test.class modified:   demo/src/demo/test.java
    Untracked files:
    demo/src/demo/ClosableLinkedBlockingDeque.java
    demo/src/demo/Counter.java
    demo/src/demo/JoinThread.java
    demo/src/demo/SectionPropertiesMap.java

--- demo/src/demo/test.java
+++ demo/src/demo/test.java
@@ -1,9 +1,12 @@
 package demo;
 
+import com.alibaba.fastjson.JSON;
 import java.util.regex.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.io.File;
+import java.util.UUID;
 
 import org.nutz.json.Json;
 import org.nutz.lang.Lang;
@@ -11,10 +14,48 @@ import java.util.Map;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
 
 public class test {
 	public static void main(String[] args){
-		SectionProperties sprop = new SectionProperties();
+		System.out.println(Timestamp.valueOf("2016-10-20 0:0:0").getTime());
+		String date = new SimpleDateFormat("yyyy-MM-dd 00:00:00").format(new Date());
+		System.out.println(date);
+		System.out.println(Timestamp.valueOf(date).getTime());
+		System.out.println("/opt/data/SAPC/_BKF/201610191205/A20161019.1205-0800-20161019.1209-0800_ebs".split("\\|"));
+		
+		Path p = Paths.get("C:\\workspace\\source\\demo");
+		System.out.println(p.toFile().getAbsolutePath());
+		
+		User user = new User();
+		user.setId(123456l);
+		user.setName("凯勒");
+		List<User> objs = JSON.parseArray("[{\"id\":123456,\"name\":\"凯勒\"},{\"id\":45678,\"name\":\"高圆圆\"}]", User.class);
+		for(User obj:objs){
+			System.out.println(obj.name);
+		}
+		
+		//System.out.println(JSON.toJSONString(user));
+		String template = "0123456789";
+		String[] strs = "NO_VALUE,,,0,,,,,".split(",",-1);
+		for(String str:strs){
+			if(str.equals(""))
+				System.out.println("test");
+		}
+		
+		System.out.println(("00000000"+"1222222222222222").substring("1222222222222222".length()));
+		
+		System.out.println(new SimpleDateFormat("yyMMdd").format(new Date()));
+		ArrayList<String> buffer = new ArrayList<String>(100);
+		
+		System.out.println(buffer.isEmpty());
+		
+		System.out.println(String.join(",", ",,,,,,,,,,,,,,123,,,,,,,,,,".split(",",Integer.MAX_VALUE)));
+		
+		SectionPropertiesMap sprop = new SectionPropertiesMap();
 		 //Properties sprop = new Properties();
 		 try{
 			 sprop.load(new FileInputStream("C:\\workspace\\source\\demo\\test.properties"));
@@ -24,8 +65,10 @@ public class test {
 		 
 		 //toFolder = C:\Users\X230\Desktop\0725\to sfs
 		 
-		 for(Object key:sprop.keySet()){
-			 System.out.println(key.toString() + " = " + sprop.getProperty(key.toString()));
+		 for(String section:sprop.getSections()){
+			 for(Object key:sprop.get(section).keySet()){
+				 System.out.println(section + "." + key.toString() + " = " + sprop.get(section).getProperty(key.toString()));
+			 }
 		 }
 		 
 		 String s = Json.toJson("{eventCode:'17',eventName:'Session_Resume',SESSION_RESUME:'SGW'}");
@@ -50,4 +93,18 @@ public class test {
 		// String g = ls.getNameGroup("sjflsjfl.l_attach.csv.gz", "(\\w+)\\.(\\w+)\\.csv\\.gz$", 1);
 		System.out.println(g);
 	}
+	
+	public static class User {
+		private Long id;
+		private String name;
+		public User(){
+			
+		}
+		public Long getId() { return id; }
+		public void setId(Long id) { this.id = id; }
+
+		public String getName() { return name; }
+		public void setName(String name) { this.name = name; }
+	}
+	
 }
