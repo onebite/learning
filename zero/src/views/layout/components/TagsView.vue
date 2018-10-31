2eba6839ac152a91c4449273599f2007e6a2c01d
liangxiaolin <lxl@lizikj.com>
2018-10-31 17:54:12 +0800

    导航条2

--- zero/src/views/layout/components/TagsView.vue
+++ zero/src/views/layout/components/TagsView.vue
@@ -3,3 +3,25 @@
         
     </div>
 </template>
+<script>
+import ScrollPane from '@/componets/ScrollPane'
+import { generateTitle } from '@/utils/i18n'
+
+export default {
+    components: { ScrollPane },
+    data() {
+        return {
+            visible: false,
+            top: 0,
+            left: 0,
+            selectedTag: {}
+        }
+    },
+    computed: {
+        visitedViews() {
+            return this.$store.state.tagsView.visitedViews
+        }
+    },
+    
+}
+</script>
