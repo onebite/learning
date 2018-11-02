490ffc61aa279fcc3237310b7f670abcebdacbf3
liangxiaolin <lxl@lizikj.com>
2018-11-02 18:01:48 +0800

    modified:   zero/src/views/layout/components/TagsView.vue

--- zero/src/views/layout/components/TagsView.vue
+++ zero/src/views/layout/components/TagsView.vue
@@ -22,6 +22,40 @@ export default {
             return this.$store.state.tagsView.visitedViews
         }
     },
-    
+    watch: {
+        $route() {
+            
+        }
+    },
+    methods: {
+        generateTitle,
+        isActive(route) {
+            return route.path = this.$route.path
+        },
+        addViewTags() {
+            const { name } = this.$route.path
+            if (name) {
+                this.$store.dispatch('addView',this.$route)
+            }
+            return false
+        },
+        moveToCurrentTag() {
+            const tags = this.$refs.tag
+            this.$nextTick(() => {
+                for (const tag of tags) {
+                    if (tag.to.path === this.$route.path) {
+                        this.$refs.ScrollPane.moveToTarget(tag.$el)
+                        if (tag.to.fullPath !== this.$route.fullPath) {
+                            this.$store.dispatch('updateVisitedView',this.$route)
+                        }
+                        break
+                    }
+                }
+            })
+        },
+        refreshSelectedTag(view) {
+            this.$store.dispatch()
+        }
+    }
 }
 </script>
